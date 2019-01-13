9fca5d8d445f7ee67b97a309232a292d27f01975
stefan00345 <stefan.ulbel@edu.fh-joanneum.at>
2019-01-13 23:51:01 +0100

    works

--- app/src/main/java/at/fh/swengb/ulbel/homeexercise2/NoteDao.kt
+++ app/src/main/java/at/fh/swengb/ulbel/homeexercise2/NoteDao.kt
@@ -1,2 +1,21 @@
 package at.fh.swengb.ulbel.homeexercise2
 
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+import android.arch.persistence.room.Update
+
+@Dao
+interface noteDao {
+    @Insert
+    fun insert(note: Note)
+    @Update
+    fun update(note: Note)
+
+    /*
+    @Query("DELETE FROM note")
+    fun deleteAllStudents()*/
+
+    @Query("SELECT * FROM notes ORDER BY title")
+    fun findAll(): List<Note>
+}
