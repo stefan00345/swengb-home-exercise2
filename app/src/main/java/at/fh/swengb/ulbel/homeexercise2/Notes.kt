c463aed272695e674ad52cb29b21c213ede4e774
stefan00345 <stefan.ulbel@edu.fh-joanneum.at>
2019-01-14 01:06:06 +0100

    final0

--- app/src/main/java/at/fh/swengb/ulbel/homeexercise2/Notes.kt
+++ app/src/main/java/at/fh/swengb/ulbel/homeexercise2/Notes.kt
@@ -3,7 +3,7 @@ package at.fh.swengb.ulbel.homeexercise2
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
-@Entity(tableName = "notes")
+@Entity(tableName = "notes") //entity
 class Note(@PrimaryKey val title: String, val content: String) {
 
 }
